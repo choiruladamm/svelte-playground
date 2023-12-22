bf64e3b2c4e56116d4e0eb6e72ace027a5fe5fb8
choiruladamm <choiruladamm@gmail.com>
2023-12-22 13:50:06 +0700

    chore: add test page

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	let name = 'test page about';
+</script>
+
+<div>
+	<h1 class="h1">{name.toUpperCase()}</h1>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae id eos ea ducimus commodi
+		soluta inventore doloribus in molestiae quae, dolorum aut omnis debitis ipsa nobis suscipit,
+		accusantium vitae maiores.
+	</p>
+</div>
+
+<style>
+	.h1 {
+		font-weight: 600;
+	}
+</style>
